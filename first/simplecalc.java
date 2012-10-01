e61bc20fc15a5bcd67da7aed13e6cb3eb9e00fb8
Dakota Strickner <dstrickner@gmail.com>
2012-10-01 12:34:08 -0300

    added multiply and divide operations fixed subtraction.

--- first/simplecalc.java
+++ first/simplecalc.java
@@ -2,7 +2,7 @@
  * A simple java calculator.
  * @author Dakota Strickner (computerxpds)
  * @version 1
- * @build 10
+ * @build 13
  * This very arcain and simple calculator will be able to do 2 variable operations. 
  */
 //import
@@ -15,7 +15,8 @@ public class simplecalc{
     public static void main(String[] args){
         boolean go = true;
         {
-            System.out.println("This is my crappy calculator! Enter a, b, c, or d, a is addition b is subtraction c is multiplication and d is division");
+            System.out.println("This is my java calculator!");
+            System.out.println("Enter a, b, c, or d, a is addition b is subtraction c is multiplication and d is division");
             
             Scanner scan = new Scanner(System.in);
             Scanner scan1 = new Scanner(System.in);
@@ -38,10 +39,27 @@ public class simplecalc{
                 int sub1 = scan.nextInt();
                 System.out.println("Now type the second number.");
                 int sub2 = scan.nextInt();
-                int sub3 = sub1 + sub2;
+                int sub3 = sub1 - sub2;
                 System.out.println(sub1 + " subtracted from " + sub2 + " equals " + sub3 + "!");
+            }
+            if("c".equals(action))
+            {
+                System.out.println("Now type in the first number you would like to multiply");
+                int mult1 = scan.nextInt();
+                System.out.println("Now type the second number");
+                int mult2 = scan.nextInt();
+                int mult3 = mult1 * mult2;
+                System.out.println("equals " + mult3);
+            }
+            if("d".equals(action))
+            {
+                System.out.println("Now type the first number you would like to divide");
+                int div1 = scan.nextInt();
+                System.out.println("Now type the second number");
+                int div2 = scan.nextInt();
+                int div3 = div1 / div2;
+                System.out.println("Equals " + div3);
             }
     }
 }
 }
-        
